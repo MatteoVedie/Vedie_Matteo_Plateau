b1f94fd5bd14afcf56cb768b347e607e17948bff
MatteoVedie <matteo.vedie2002@gmail.com>
2024-11-28 22:45:52 +0100

    oskour

---
+++ Source/GD3_CPP/GM_Collectible.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GM_Collectible.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GD3_CPP_API AGM_Collectible : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
