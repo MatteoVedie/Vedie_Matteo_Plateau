1bd35c559bc2b7cdc98439997834b287dffe5bf4
MatteoVedie <matteo.vedie2002@gmail.com>
2025-01-20 16:58:52 +0100

    Start MasterMind

---
+++ Source/GD3_CPP/MyMasterMind.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyMasterMind.h"
+
+// Sets default values
+AMyMasterMind::AMyMasterMind()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void AMyMasterMind::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AMyMasterMind::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
